3d2c023c3a34efb2b0a7beb78f0e3a4daa084955
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2021-01-04 18:59:13 +0100

    Add debug page for checking local rest api endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import os
+
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -21,8 +22,10 @@ def create_app(options={}):
         )
     app = Flask(__name__)
     app.config.from_object(config_name)
+    add_debug_page(app)
     init_extensions(app)
     register_blueprints(app)
+
     return app
 
 
@@ -36,3 +39,21 @@ def init_extensions(app):
 def register_blueprints(app):
     from app.restapi import api
     app.register_blueprint(api, url_prefix='/api')
+
+
+def add_debug_page(app):
+    @app.route('/')
+    def index():
+        content = """Endpoints:</br>             
+         <a href="/api/categories">/api/categories</a></br>
+         <a href="/api/countries">/api/countries</a></br>
+         <a href="/api/customers">/api/customers</a></br>
+         <a href="/api/products">/api/products</a></br>
+         <a href="/api/orders">/api/orders</a></br>
+         Statistics:</br>
+         <a href="/api/statistics/sells_by_product">/api/statistics/sells_by_product</a></br>
+         <a href="/api/statistics/products_by_category">/api/statistics/products_by_category</a></br>
+         <a href="/api/statistics/sells_by_product">/api/statistics/sells_by_product</a></br>
+         <a href="/api/statistics/units_delivered_by_product_by_country">/api/statistics/units_delivered_by_product_by_country</a></br>
+         """
+        return content
